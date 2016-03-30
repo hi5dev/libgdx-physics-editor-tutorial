ddbbf8c1990558367a5dd0cac6262277eaa74a28
Travis Haynes <travis@hi5dev.com>
2016-03-30 13:05:17 -0700

    reformatted code

--- android/src/com/hi5dev/physics/AndroidLauncher.java
+++ android/src/com/hi5dev/physics/AndroidLauncher.java
@@ -4,13 +4,12 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.hi5dev.physics.PhysicsGame;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new PhysicsGame(), config);
-	}
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+    initialize(new PhysicsGame(), config);
+  }
 }
