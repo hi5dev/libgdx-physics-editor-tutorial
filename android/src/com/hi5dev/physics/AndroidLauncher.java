03f64431de55eeab0bb9f1b2f8ab0d4afc174aa1
Travis Haynes <travis@hi5dev.com>
2016-03-30 11:52:18 -0700

    initial

---
+++ android/src/com/hi5dev/physics/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.hi5dev.physics;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.hi5dev.physics.PhysicsGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PhysicsGame(), config);
+	}
+}
---
+++ desktop/src/com/hi5dev/physics/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hi5dev.physics.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hi5dev.physics.PhysicsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PhysicsGame(), config);
+	}
+}
